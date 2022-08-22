6c81dd35ba78cbf6adff5ee23f10993c0d5f7953
monyyn <96463913+monyyn@users.noreply.github.com>
2022-08-22 15:26:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ today = datetime.now()
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
 birthday = os.environ['BIRTHDAY']
-words = os.environ['WORDS']
 
 app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
